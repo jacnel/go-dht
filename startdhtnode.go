cde4a6e45a4c91eedb4176a59e5dad1954e1ff68
Jacob Nelson <jnelson3225@gmail.com>
2018-02-17 15:55:39 -0500

    program to launch node

---
+++ startdhtnode.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	dhtnode "dht/dhtnode"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("Please provide a portnumber to start the node.")
+	}
+	arg := os.Args[1]
+	var port int
+	if strings.Compare(arg, "") != 0 {
+		var err error
+		port, err = strconv.Atoi(arg)
+		if err != nil {
+			panic("Invalid portnumber... exiting.")
+		}
+	}
+	dhtNode := dhtnode.DHTNode{}
+	dhtNode.StartNode(port, "./config.json")
+}
