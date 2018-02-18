bb731c3eef48302df2b301de8e8c97f49a136e19
Jacob Nelson <jnelson3225@gmail.com>
2018-02-18 15:09:49 -0500

    debug test scripts

--- node/test-nodes.sh
+++ node/test-nodes.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
-printf "cd go/src/dht/node/\nsh launch-node.sh & exit\n" | ssh -i dht-testing.pem ec2-user@ec2-54-208-29-162.compute-1.amazonaws.com
+printf "cd go/src/dht/node/\nsh launch-node.sh & exit\n" | ssh -i dht-testing.pem ec2-user@ec2-54-208-29-162.compute-1.amazonaws.com &
 
+printf "cd go/src/dht/node/\nsh launch-node.sh & exit\n" | ssh -i dht-testing.pem ec2-user@ec2-54-211-127-45.compute-1.amazonaws.com &
 
-printf "cd go/src/dht/node/\nsh launch-node.sh & exit\n" | ssh -i dht-testing.pem ec2-user@ec2-54-211-127-45.compute-1.amazonaws.com
+printf "cd go/src/dht/node/\nsh launch-node.sh & exit\n" | ssh -i dht-testing.pem ec2-user@ec2-75-101-226-165.compute-1.amazonaws.com &
 
-printf "cd go/src/dht/node/\nsh launch-node.sh & exit\n" | ssh -i dht-testing.pem ec2-user@ec2-75-101-226-165.compute-1.amazonaws.com
-
-printf "cd go/src/dht/node/\nsh launch-node.sh & exit\n" | ssh -i dht-testing.pem ec2-user@ec2-34-233-120-248.compute-1.amazonaws.com
+printf "cd go/src/dht/node/\nsh launch-node.sh & exit\n" | ssh -i dht-testing.pem ec2-user@ec2-34-233-120-248.compute-1.amazonaws.com &
 
 go build testnode.go
 
