71ce5618e74171ed8e8ba8447e20716e67f94816
Jacob Nelson <jnelson3225@gmail.com>
2018-02-18 21:03:40 -0500

    seed random number with current time

--- node/testnode.go
+++ node/testnode.go
@@ -30,6 +30,8 @@ func doRandomWork(i, targetNode, numOps, keyRange int, ops, puts *[]int, runtime
 	myPuts := 0
 	myOps := 0
 	var nanos int64
+	now := time.Now()
+	rand2.Seed(time.Since(now).Nanoseconds())
 	for j := 0; j < numOps; j++ {
 		if j % 50 == 0 {
 			fmt.Println("client:", i, "j:", j, "myPuts:", myPuts)
